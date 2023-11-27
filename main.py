e4c24dfb5d0a1995f9e65d341b8a0e76ba65d4e1
parvizx3 <129318294+parvizx3@users.noreply.github.com>
2023-11-27 14:57:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import requests
-import re
-import json
-import random
-import string
-
 
 def fetch(url):
-    # Fetch the JSON from the provided URL
-    response = requests.get(url)
-    data = response.json()
+    try:
+        response = requests.get(url)
 
-    return data
-
-def save_to_file(data, filename='mix.json'):
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+        # Check if the request was successful
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: Received status code {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode JSON: {e}")
+        print("Response content:", response.text)
+        return None
 
 url = 'https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/singbox/sfasfi/mix.json'
 cleaned_data = fetch(url)
 
-# Save the cleaned JSON data to a new file
-save_to_file(cleaned_data)
+if cleaned_data is not None:
+    save_to_file(cleaned_data)
+else:
+    print("No data to save.")
